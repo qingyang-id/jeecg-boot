d2544721a0a24fe0faabd29db39a142c08dc0d6d
yq <qing.yang@binance.com>
2022-04-03 15:40:53 +0800

    update token time

--- jeecg-boot/jeecg-boot-base/jeecg-boot-base-core/src/main/java/org/jeecg/common/system/util/JwtUtil.java
+++ jeecg-boot/jeecg-boot-base/jeecg-boot-base-core/src/main/java/org/jeecg/common/system/util/JwtUtil.java
@@ -33,8 +33,8 @@ import java.util.Date;
  **/
 public class JwtUtil {
 
-	// Token过期时间30分钟（用户登录过期时间是此时间的两倍，以token在reids缓存时间为准）
-	public static final long EXPIRE_TIME = 48 * 60 * 60 * 1000;
+	// Token过期时间30天（用户登录过期时间是此时间的两倍，以token在reids缓存时间为准）
+	public static final long EXPIRE_TIME = 30 * 48 * 60 * 60 * 1000;
 
     /**
      *
